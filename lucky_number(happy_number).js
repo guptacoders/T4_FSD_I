37b625907b69c19970d23714d5d98fd719b427eb
LJ_PRO-CODERS <guptacoders@gmail.com>
2025-01-07 10:41:06 +0530

    lucky_number(happy_number).js

--- lucky_number(happy_number).js
+++ lucky_number(happy_number).js
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script language="javascript" type="text/javascript">
+      function lucky(num) {
+        while (num > 9) {
+          let sum = 0;
+          while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+          }
+          num = sum;
+        }
 
+        if (num === 1) {
+          return "Lucky Number";
+        } else {
+          return "Not a Lucky Number";
+        }
+      }
+
+      document.write(lucky(7777) + "<br>");
+    
+    </script>
+  </body>
+</html>
